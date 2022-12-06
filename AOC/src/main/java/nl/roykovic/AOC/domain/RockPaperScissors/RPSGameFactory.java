2b25827e639538ad242d2cc6237025cef19e5460
Roykovic <roykovic97@outlook.com>
2022-12-06 13:44:53 +0100

    added factory for RPS games

---
+++ AOC/src/main/java/nl/roykovic/AOC/domain/RockPaperScissors/RPSGameFactory.java
@@ -0,0 +1,31 @@
+package nl.roykovic.AOC.domain.RockPaperScissors;
+
+import nl.roykovic.AOC.domain.Elf;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RPSGameFactory {
+    public List<RPSGame> generateFromFile(File file) throws FileNotFoundException {
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        List<String> lines = reader.lines().toList();
+
+        List<RPSGame> gameList = new ArrayList<>();
+
+        for(String line: lines){
+            char[] moves = StringUtils.deleteWhitespace(line).toCharArray();
+
+            RPSGame game = new RPSGame(moves[0], moves[1]);
+            gameList.add(game);
+        }
+        return gameList;
+    }
+}

85d97186a3a95561575dc72c46c6fffde138e0d3
Roykovic <roykovic97@outlook.com>
2022-12-06 14:04:31 +0100

    added character to RPSOutcome for solution 2

--- AOC/src/main/java/nl/roykovic/AOC/domain/RockPaperScissors/RPSOutcome.java
+++ AOC/src/main/java/nl/roykovic/AOC/domain/RockPaperScissors/RPSOutcome.java
@@ -1,17 +1,28 @@
 package nl.roykovic.AOC.domain.RockPaperScissors;
 
 public enum RPSOutcome {
-    LOSS(0),
-    DRAW(3),
-    WIN(6);
+    LOSS(0, 'X'),
+    DRAW(3, 'Y'),
+    WIN(6, 'Z');
 
     private final int score;
+    private final char character;
 
-    RPSOutcome(int score) {
+    RPSOutcome(int score, char character) {
         this.score = score;
+        this.character = character;
     }
 
     public int getScore() {
         return score;
+    }
+
+    public static RPSOutcome getByChar(char character){
+        for(RPSOutcome outcome : values()){
+            if(outcome.character == character || outcome.character == character){
+                return outcome;
+            }
+        }
+        throw new IllegalArgumentException(String.valueOf(character));
     }
 }

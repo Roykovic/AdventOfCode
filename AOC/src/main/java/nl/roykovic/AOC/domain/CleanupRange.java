0bf4039cbc7dd6a61a4b2551f41caa7f99653933
Roykovic <roykovic97@outlook.com>
2022-12-06 16:00:12 +0100

    Create CleanupRange.java

---
+++ AOC/src/main/java/nl/roykovic/AOC/domain/CleanupRange.java
@@ -0,0 +1,15 @@
+package nl.roykovic.AOC.domain;
+
+public class CleanupRange {
+    private final int start;
+    private final int end;
+
+    public CleanupRange(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public boolean contains(CleanupRange otherRange){
+        return start <= otherRange.start && end >= otherRange.end;
+    }
+}

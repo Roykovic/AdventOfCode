c1519ae136bed81a284a745deea73f1d863f2a9c
Roykovic <roykovic97@outlook.com>
2022-12-06 14:05:11 +0100

    Added test for solution 2

--- AOC/src/test/java/nl/roykovic/AOC/RPSGameFactoryTest.java
+++ AOC/src/test/java/nl/roykovic/AOC/RPSGameFactoryTest.java
@@ -16,7 +16,22 @@ public class RPSGameFactoryTest {
     @Test
     void testFactory() throws IOException {
         File input = new ClassPathResource("RockPaperScissorInput.txt").getFile();
-        List<RPSGame> list = new RPSGameFactory().generateFromFile(input);
+        List<RPSGame> list = new RPSGameFactory().generateFromFile(input, false);
+
+        int score = 0;
+
+        for(RPSGame game : list){
+            score += game.determineScore();
+        }
+
+        System.out.println(list.size());
+        System.out.println(score);
+    }
+
+    @Test
+    void testFactory2() throws IOException {
+        File input = new ClassPathResource("RockPaperScissorInput.txt").getFile();
+        List<RPSGame> list = new RPSGameFactory().generateFromFile(input, true);
 
         int score = 0;
 

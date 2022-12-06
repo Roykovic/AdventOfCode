8d671f408bdc9c7f11cb92a7553ce13e4dc33866
Roykovic <roykovic97@outlook.com>
2022-12-06 13:44:46 +0100

    added RPS game

---
+++ AOC/src/main/java/nl/roykovic/AOC/domain/RockPaperScissors/RPSGame.java
@@ -0,0 +1,33 @@
+package nl.roykovic.AOC.domain.RockPaperScissors;
+
+import java.util.Map;
+
+import static nl.roykovic.AOC.domain.RockPaperScissors.RPSMove.WINS;
+
+public class RPSGame {
+    private final RPSMove opponentsMove;
+    private final RPSMove myMove;
+
+    public RPSGame(char opponentsMove, char myMove){
+        this(RPSMove.getByChar(opponentsMove), RPSMove.getByChar(myMove));
+    }
+
+    public RPSGame(RPSMove opponentsMove, RPSMove myMove) {
+        this.opponentsMove = opponentsMove;
+        this.myMove = myMove;
+    }
+
+    private RPSOutcome determineOutcome(){
+        if(opponentsMove.equals(myMove)){
+            return RPSOutcome.DRAW;
+        }
+        if(WINS.get(myMove).equals(opponentsMove)){
+            return RPSOutcome.WIN;
+        }
+        return RPSOutcome.LOSS;
+    }
+
+    public int determineScore(){
+        return determineOutcome().getScore() + myMove.getScore();
+    }
+}

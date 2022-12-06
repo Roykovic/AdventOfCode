b564980bdd7cfc6b15475e9ed6b00862b560da46
Roykovic <roykovic97@outlook.com>
2022-12-06 17:18:24 +0100

    Create CraneInstruction.java

---
+++ AOC/src/main/java/nl/roykovic/AOC/domain/SupplyStacks/CraneInstruction.java
@@ -0,0 +1,25 @@
+package nl.roykovic.AOC.domain.SupplyStacks;
+
+public class CraneInstruction {
+    private final int amountOfCrates;
+    private final int originStack;
+    private final int destinationStack;
+
+    public CraneInstruction(int amountOfCrates, int originStack, int destinationStack) {
+        this.amountOfCrates = amountOfCrates;
+        this.originStack = originStack;
+        this.destinationStack = destinationStack;
+    }
+
+    public int getAmountOfCrates() {
+        return amountOfCrates;
+    }
+
+    public int getOriginStack() {
+        return originStack;
+    }
+
+    public int getDestinationStack() {
+        return destinationStack;
+    }
+}

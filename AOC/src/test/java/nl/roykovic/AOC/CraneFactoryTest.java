b016990011d59da0d3b8babbf9956967960c17b4
Roykovic <roykovic97@outlook.com>
2022-12-06 17:18:37 +0100

    Create CraneFactoryTest.java

---
+++ AOC/src/test/java/nl/roykovic/AOC/CraneFactoryTest.java
@@ -0,0 +1,26 @@
+package nl.roykovic.AOC;
+
+import nl.roykovic.AOC.domain.Elf;
+import nl.roykovic.AOC.domain.ElfFactory;
+import nl.roykovic.AOC.domain.SupplyStacks.Crane;
+import nl.roykovic.AOC.domain.SupplyStacks.CraneFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+public class CraneFactoryTest {
+    @Test
+    void testFactory() throws IOException {
+        File input = new ClassPathResource("CrateInput.txt").getFile();
+        Crane crane = new CraneFactory().generateFromFile(input);
+
+        crane.executeInstructions();
+
+        crane.printTopCrates();
+
+    }
+}

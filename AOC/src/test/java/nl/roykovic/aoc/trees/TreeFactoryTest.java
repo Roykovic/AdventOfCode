7d0b8cdb9b7c73c3238d8f0fd72ed70df219699a
Roykovic <roykovic97@outlook.com>
2022-12-08 15:24:47 +0100

    fixed treefactorytest

--- AOC/src/test/java/nl/roykovic/aoc/trees/TreeFactoryTest.java
+++ AOC/src/test/java/nl/roykovic/aoc/trees/TreeFactoryTest.java
@@ -7,25 +7,46 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TreeFactoryTest {
     @Test
-    void testFactory() throws IOException {
+    void testAmountOfVisibleExampleTrees() throws IOException {
+        File input = new File("src/test/resources/TreeExampleInput.txt");
+        TreePatch trees = new TreeFactory().generateFromFile(input);
+
+        Tree[] visibleTrees = Arrays.stream(trees.getPatch()).flatMap(Arrays::stream).filter(Tree::isVisible).toArray(Tree[]::new);
+
+        assertEquals(21, visibleTrees.length);
+
+    }
+
+    @Test
+    void testAmountOfVisibleActualTrees() throws IOException {
         File input = new ClassPathResource("TreesInput.txt").getFile();
         TreePatch trees = new TreeFactory().generateFromFile(input);
 
         Tree[] visibleTrees = Arrays.stream(trees.getPatch()).flatMap(Arrays::stream).filter(Tree::isVisible).toArray(Tree[]::new);
 
-        System.out.println(visibleTrees.length);
+        assertEquals(1719, visibleTrees.length);
 
     }
 
     @Test
-    void testFactory2() throws IOException {
+    void testHighestScenicExampleScore() throws IOException {
+        File input = new File("src/test/resources/TreeExampleInput.txt");
+        TreePatch trees = new TreeFactory().generateFromFile(input);
+
+        int highestScenicScore = Arrays.stream(trees.getPatch()).flatMap(Arrays::stream).mapToInt(Tree::getScenicScore).max().orElse(0);
+        assertEquals(8, highestScenicScore);
+    }
+
+    @Test
+    void testHighestScenicActualScore() throws IOException {
         File input = new ClassPathResource("TreesInput.txt").getFile();
         TreePatch trees = new TreeFactory().generateFromFile(input);
 
         int highestScenicScore = Arrays.stream(trees.getPatch()).flatMap(Arrays::stream).mapToInt(Tree::getScenicScore).max().orElse(0);
-        System.out.println(highestScenicScore);
-
+        assertEquals(590824, highestScenicScore);
     }
 }

33471ba746a31537c1bddb77c4afe4a694588d81
Roykovic <roykovic97@outlook.com>
2022-12-08 12:55:38 +0100

    added scenic score to treepatch

--- AOC/src/main/java/nl/roykovic/aoc/trees/TreePatch.java
+++ AOC/src/main/java/nl/roykovic/aoc/trees/TreePatch.java
@@ -10,6 +10,7 @@ public class TreePatch {
     public TreePatch(Tree[][] patch) {
         this.patch = patch;
         setVisible();
+        setScenicScores();
     }
 
     private void setVisible(){
@@ -20,6 +21,7 @@ public class TreePatch {
             checkVisibility(row);
             ArrayUtils.reverse(row);
             checkVisibility(row);
+            ArrayUtils.reverse(row);
         }
 
         for(int columnIndex = 0; columnIndex < patch[0].length; columnIndex++){
@@ -28,6 +30,15 @@ public class TreePatch {
             checkVisibility(column);
             ArrayUtils.reverse(column);
             checkVisibility(column);
+            ArrayUtils.reverse(column);
+        }
+    }
+
+    private void setScenicScores(){
+        for (Tree[] trees : patch) {
+            for (Tree tree : trees) {
+                setScenicScore(tree);
+            }
         }
     }
 
@@ -47,6 +58,37 @@ public class TreePatch {
                 highestTree = tree.getHeight();
             }
         }
+    }
+
+    private void setScenicScore(Tree tree){
+        int scenicScore = 1;
+
+        Tree[] treeRow = getRow(tree.getY());
+        Tree[] treeCol = getColumn(tree.getX());
+        scenicScore *= getViewDistance(treeRow, tree.getX(), tree.getHeight());
+        scenicScore *= getViewDistance(treeCol, tree.getY(), tree.getHeight());
+
+        ArrayUtils.reverse(treeRow);
+        ArrayUtils.reverse(treeCol);
+        scenicScore *= getViewDistance(treeRow, (treeRow.length - tree.getX()) -1, tree.getHeight());
+        scenicScore *= getViewDistance(treeCol, (treeRow.length - tree.getY()) -1, tree.getHeight());
+
+        ArrayUtils.reverse(treeRow);
+        ArrayUtils.reverse(treeCol);
+
+        tree.setScenicScore(scenicScore);
+    }
+
+    private int getViewDistance(Tree[] treeline, int pos, int height){
+        int viewDistance = 0;
+        int curTreeHeight = -1;
+        while(pos +1 < treeline.length && curTreeHeight < height ){
+            Tree curTree = treeline[pos+1];
+            curTreeHeight = curTree.getHeight();
+            viewDistance++;
+            pos++;
+        }
+        return viewDistance;
     }
 
     public Tree[][] getPatch() {

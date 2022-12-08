828ff8202f40b04bb19666138a92e5140aa54509
Roykovic <roykovic97@outlook.com>
2022-12-08 13:36:14 +0100

    fixed crane tests

--- AOC/src/test/java/nl/roykovic/aoc/supplystacks/CraneFactoryTest.java
+++ AOC/src/test/java/nl/roykovic/aoc/supplystacks/CraneFactoryTest.java
@@ -1,33 +1,67 @@
 package nl.roykovic.aoc.supplystacks;
 
-import nl.roykovic.aoc.supplystacks.Crane;
-import nl.roykovic.aoc.supplystacks.CraneFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CraneFactoryTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    @BeforeEach
+    public void setUpStreams(){
+        System.setOut(new PrintStream(outContent));
+    }
     @Test
-    void testFactory() throws IOException {
+    void test9000CrateExampleOutput() throws IOException {
+        File input = new File("src/test/resources/CrateTestInput.txt");
+        Crane crane = new CraneFactory().generateFromFile(input);
+
+        crane.executeInstructions();
+
+        crane.printTopCrates();
+        assertEquals("CMZ", outContent.toString());
+
+    }
+
+    @Test
+    void test9000CrateActualOutput() throws IOException {
         File input = new ClassPathResource("CrateInput.txt").getFile();
         Crane crane = new CraneFactory().generateFromFile(input);
 
         crane.executeInstructions();
 
         crane.printTopCrates();
+        assertEquals("RTGWZTHLD", outContent.toString());
 
     }
     @Test
     void testFactory2() throws IOException {
+        File input = new File("src/test/resources/CrateTestInput.txt");
+        Crane crane = new CraneFactory().generateFromFile(input);
+
+        crane.executeImprovedInstructions();
+
+        crane.printTopCrates();
+        assertEquals("MCD", outContent.toString());
+    }
+
+    @Test
+    void test9001CrateActualOutput() throws IOException {
         File input = new ClassPathResource("CrateInput.txt").getFile();
         Crane crane = new CraneFactory().generateFromFile(input);
 
         crane.executeImprovedInstructions();
 
         crane.printTopCrates();
-
+        assertEquals("STHGRZZFR", outContent.toString());
     }
 
 }

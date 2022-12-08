d1a07948e7c9dcb23f7b7fde8c2b0be0aae8e24b
Roykovic <roykovic97@outlook.com>
2022-12-08 13:19:39 +0100

    fixed tests to actually test things

--- AOC/src/test/java/nl/roykovic/aoc/elf/ElfFactoryTest.java
+++ AOC/src/test/java/nl/roykovic/aoc/elf/ElfFactoryTest.java
@@ -10,18 +10,57 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ElfFactoryTest {
 
     @Test
-    void testFactory() throws IOException {
+    void testExampleCalories() throws IOException {
+        File input = new File("src/test/resources/elf/ElfTestInput.txt");
+        List<Elf> list = new ElfFactory().generateFromFile(input);
+
+        Collections.sort(list);
+        Collections.reverse(list);
+
+        assertEquals(24000, list.get(0).getCalories());
+    }
+
+    @Test
+    void testActualCalories() throws IOException {
         File input = new ClassPathResource("ElfCalorieInput.txt").getFile();
         List<Elf> list = new ElfFactory().generateFromFile(input);
 
-        System.out.println(list.size());
-        System.out.println(list);
         Collections.sort(list);
         Collections.reverse(list);
-        System.out.println(list);
+
+        assertEquals(70116, list.get(0).getCalories());
+
+    }
+
+    @Test
+    void testExampleTopThreeCalories() throws IOException {
+        File input = new File("src/test/resources/elf/ElfTestInput.txt");
+        List<Elf> list = new ElfFactory().generateFromFile(input);
+
+        Collections.sort(list);
+        Collections.reverse(list);
+
+        long topThree = list.subList(0, 3).stream().mapToLong(Elf::getCalories).reduce(0, Long::sum);
+
+        assertEquals(45000, topThree);
+    }
+
+    @Test
+    void testActualTopThreeCalories() throws IOException {
+        File input = new ClassPathResource("ElfCalorieInput.txt").getFile();
+        List<Elf> list = new ElfFactory().generateFromFile(input);
+
+        Collections.sort(list);
+        Collections.reverse(list);
+
+        long topThree = list.subList(0, 3).stream().mapToLong(Elf::getCalories).reduce(0, Long::sum);
+
+        assertEquals(206582, topThree);
 
     }
 }

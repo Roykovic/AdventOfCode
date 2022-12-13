8c476786b84f4be67b5604c3ba7634941963c0fe
Roykovic <roykovic97@outlook.com>
2022-12-13 12:31:43 +0100

    added factory using jackson objectmapper

---
+++ AOC/src/main/java/nl/roykovic/aoc/distresssignal/PacketFactory.java
@@ -0,0 +1,49 @@
+package nl.roykovic.aoc.distresssignal;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nl.roykovic.aoc.rucksack.Rucksack;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.NotImplementedException;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class PacketFactory {
+    public LinkedHashMap<ArrayList, ArrayList> generateFromFile(File file) throws FileNotFoundException, JsonProcessingException {
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        List<String> lines = reader.lines().toList();
+
+        LinkedHashMap<ArrayList, ArrayList> dataMap = new LinkedHashMap<>();
+
+        ArrayList left = null;
+
+        for(String line: lines){
+
+            if(!StringUtils.isBlank(line)) {
+                ObjectMapper mapper = new ObjectMapper();
+                ArrayList packetData = mapper.readValue(line, new TypeReference<>() {});
+
+                if(left == null){
+                    left = packetData;
+                }
+                else{
+                    dataMap.put(left, packetData);
+                }
+            }
+            else{
+                left = null;
+            }
+        }
+        return dataMap;
+    }
+}

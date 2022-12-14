8ce666b233391cf6185158548ded347620d7408d
Roykovic <roykovic97@outlook.com>
2022-12-14 14:40:15 +0100

    fixed factory to use map

--- AOC/src/main/java/nl/roykovic/aoc/fallingsand/FallingSandFactory.java
+++ AOC/src/main/java/nl/roykovic/aoc/fallingsand/FallingSandFactory.java
@@ -1,37 +1,37 @@
 package nl.roykovic.aoc.fallingsand;
 
-import nl.roykovic.aoc.rucksack.Rucksack;
 import nl.roykovic.aoc.utils.Coord;
-import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FallingSandFactory {
-    public List<IParticle> generateFromFile(File file) throws FileNotFoundException {
+
+    private int highestY;
+    public Cave generateFromFile(File file) throws FileNotFoundException {
 
         BufferedReader reader = new BufferedReader(new FileReader(file));
 
-        List<String> lines = reader.lines().distinct().toList();
-        List<IParticle> rocks = new ArrayList<>();
+        HashMap<Coord, IParticle> particles = new HashMap<Coord, IParticle>(
+                reader.lines().distinct()
+                        .map(it -> it.split(" -> "))
+                        .flatMap(coordinates -> connectRocks(
+                                Arrays.stream(coordinates)
+                                        .map(coordinate -> new Rock(new Coord(coordinate))))).distinct().collect(Collectors.toMap(IParticle::getCoord, Function.identity())));
 
-        for(String line: lines){
-            String[] coordinates = line.split(" -> ");
-            List<Rock> rockList = new ArrayList<>();
-            for(String coordinate : coordinates){
-                rockList.add(new Rock(new Coord(coordinate)));
-            }
-            rocks.addAll(connectRocks(rockList));
-        }
-        return rocks;
+        return new Cave(particles, highestY);
     }
 
-    private List<Rock> connectRocks(List<Rock> rocks){
-        List<Rock> returnList = new ArrayList<>();
+    private Stream<Rock> connectRocks(Stream<Rock> rockStream){
+        Set<Rock> returnList = new HashSet<>();
+        List<Rock> rocks = rockStream.toList();
+
         for(int i = 1; i < rocks.size(); i++){
             Rock first = rocks.get(i -1);
             Rock second = rocks.get(i);
@@ -41,15 +41,21 @@ public class FallingSandFactory {
             int smallestY = Math.min(first.getCoord().getY(), second.getCoord().getY());
             int biggestY = Math.max(first.getCoord().getY(), second.getCoord().getY());
 
+            if(biggestY > highestY){
+                highestY = biggestY;
+            }
+
             for(int x = smallestX +1; x < biggestX; x++){
-                returnList.add(new Rock(new Coord(x, smallestY)));
+                Coord coord = new Coord(x, smallestY);
+                returnList.add(new Rock(coord));
             }
             for(int y = smallestY +1; y < biggestY; y++){
-                returnList.add(new Rock(new Coord(smallestX, y)));
+                Coord coord = new Coord(smallestX, y);
+                returnList.add(new Rock(coord));
             }
         }
 
         returnList.addAll(rocks);
-        return returnList;
+        return returnList.stream();
     }
 }

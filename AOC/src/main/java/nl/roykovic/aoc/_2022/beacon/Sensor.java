92fb2df808fe8b4bd6e7c69faf1d2b1795f398f7
Roykovic <roykovic97@outlook.com>
2022-12-15 15:39:09 +0100

    added method to get all coords just out of range for sensor

--- AOC/src/main/java/nl/roykovic/aoc/_2022/beacon/Sensor.java
+++ AOC/src/main/java/nl/roykovic/aoc/_2022/beacon/Sensor.java
@@ -2,6 +2,11 @@ package nl.roykovic.aoc._2022.beacon;
 
 import nl.roykovic.aoc.utils.Coord;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Sensor {
     private final Coord coord;
     private final Beacon closestBeacon;
@@ -13,6 +18,30 @@ public class Sensor {
 
     public long manhattanDistanceToBeacon(){
      return coord.manhattanDistance(closestBeacon.getCoord());
+    }
+
+    List<Coord> signalRangePerimeter(){
+        Set<Coord> perimeter = new HashSet<>();
+        long upperY = getCoord().getY();
+        long lowerY = getCoord().getY();
+        for(long x = coord.getX() - manhattanDistanceToBeacon() -1; x < coord.getX() + manhattanDistanceToBeacon() +2; x++){
+
+            perimeter.add(new Coord(x, upperY));
+
+            if(upperY != lowerY){
+                perimeter.add(new Coord(x, lowerY));
+            }
+
+            if(x < coord.getX()){
+                upperY--;
+                lowerY++;
+            }
+            else{
+                upperY++;
+                lowerY--;
+            }
+        }
+        return perimeter.stream().toList();
     }
 
     public boolean isInSignalRange(Coord coord){

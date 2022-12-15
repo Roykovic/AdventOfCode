1b7035b00af8fcf43a8219f2de723364f3c27797
Roykovic <roykovic97@outlook.com>
2022-12-15 13:37:02 +0100

    added beacon and sensor

---
+++ AOC/src/main/java/nl/roykovic/aoc/_2022/beacon/Beacon.java
@@ -0,0 +1,15 @@
+package nl.roykovic.aoc._2022.beacon;
+
+import nl.roykovic.aoc.utils.Coord;
+
+public class Beacon {
+    private final Coord coord;
+
+    public Beacon(Coord coord) {
+        this.coord = coord;
+    }
+
+    public Coord getCoord() {
+        return coord;
+    }
+}
---
+++ AOC/src/main/java/nl/roykovic/aoc/_2022/beacon/Sensor.java
@@ -0,0 +1,25 @@
+package nl.roykovic.aoc._2022.beacon;
+
+import nl.roykovic.aoc.utils.Coord;
+
+public class Sensor {
+    private final Coord coord;
+    private final Beacon closestBeacon;
+
+    public Sensor(Coord coord, Beacon closestBeacon) {
+        this.coord = coord;
+        this.closestBeacon = closestBeacon;
+    }
+
+    public long manhattanDistanceToBeacon(){
+     return coord.manhattanDistance(closestBeacon.getCoord());
+    }
+
+    public Beacon getClosestBeacon() {
+        return closestBeacon;
+    }
+
+    public Coord getCoord() {
+        return coord;
+    }
+}

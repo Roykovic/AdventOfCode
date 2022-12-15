a8cd55e937eeb643699152df0dbf4eb6adbd659c
Roykovic <roykovic97@outlook.com>
2022-12-15 19:17:27 +0100

    fixed factory and test

--- AOC/src/main/java/nl/roykovic/aoc/_2015/present/HouseFactory.java
+++ AOC/src/main/java/nl/roykovic/aoc/_2015/present/HouseFactory.java
@@ -8,6 +8,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class HouseFactory {
@@ -22,10 +23,28 @@ public class HouseFactory {
 
         char[] instructions = input.toCharArray();
         List<Coord> houseCoords = new ArrayList<>(List.of(new Coord(0L, 0L)));
-        for(char instruction: instructions){
-
+        for(int i = 0; i < instructions.length; i++){
+            houseCoords.add(instructionToCoord(houseCoords.get(i), instructions[i]));
         }
-        
-        return null;
+
+        return houseCoords.stream().distinct().collect(Collectors.toList());
+    }
+
+    private Coord instructionToCoord(Coord previousCoord, char instruction){
+        switch(instruction){
+            case '^' -> {
+                return new Coord(previousCoord.getX(), previousCoord.getY() -1);
+            }
+            case '>' -> {
+                return new Coord(previousCoord.getX() +1, previousCoord.getY());
+            }
+            case 'v' -> {
+                return new Coord(previousCoord.getX(), previousCoord.getY() +1);
+            }
+            case '<' -> {
+                return new Coord(previousCoord.getX() -1, previousCoord.getY());
+            }
+            default -> throw new IllegalArgumentException(previousCoord + " " + instruction);
+        }
     }
 }
--- AOC/src/test/java/nl/roykovic/aoc/_2015/present/HouseFactoryTest.java
+++ AOC/src/test/java/nl/roykovic/aoc/_2015/present/HouseFactoryTest.java
@@ -1,8 +1,16 @@
 package nl.roykovic.aoc._2015.present;
 
 import nl.roykovic.aoc._2015.apartment.ApartmentFactory;
+import nl.roykovic.aoc.utils.Coord;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,7 +22,13 @@ public class HouseFactoryTest {
             "^v^v^v^v^v,2"
     })
     void testExampleHouses(String input, int expectedNumberOfHouses) {
-        int floor = new ApartmentFactory().generateFromString(input).sum();
-        assertEquals(expectedNumberOfHouses, floor);
+        int houses = new HouseFactory().generateFromString(input).size();
+        assertEquals(expectedNumberOfHouses, houses);
+    }
+    @Test
+    void testActualHouses() throws IOException {
+        File input = new ClassPathResource("2015/HouseInput.txt").getFile();
+        int houses = new HouseFactory().generateFromFile(input).size();
+        assertEquals(2565, houses);
     }
 }

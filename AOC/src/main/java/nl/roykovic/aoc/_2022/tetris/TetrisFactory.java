cf0621864d64fc4ac3e5f8cb14126db3ede7f389
Roykovic <roykovic97@outlook.com>
2022-12-19 15:47:38 +0100

    test + answer

--- AOC/src/main/java/nl/roykovic/aoc/_2022/tetris/TetrisFactory.java
+++ AOC/src/main/java/nl/roykovic/aoc/_2022/tetris/TetrisFactory.java
@@ -1,4 +1,94 @@
 package nl.roykovic.aoc._2022.tetris;
 
+import nl.roykovic.aoc.utils.Coord;
+import nl.roykovic.aoc.utils.Direction;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+
 public class TetrisFactory {
+    public Long generateFromFile(File file) throws FileNotFoundException {
+        List<TetrisRockType> pieces = List.of(TetrisRockType.LINE, TetrisRockType.PLUS, TetrisRockType.L, TetrisRockType.COLUMN, TetrisRockType.SQUARE);
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        char[] instructions = reader.lines().findFirst().orElseThrow(IllegalArgumentException::new).toCharArray();
+
+        long highestBlock = 0;
+
+        List<TetrisRock> rocks = new ArrayList<>();
+
+        int rockType = 0;
+        int instructionPos = 0;
+
+        for(int i = 0; i< 2022; i++){
+            TetrisRock rock = new TetrisRock(pieces.get(rockType), new Coord(2L,(highestBlock - 3) - pieces.get(rockType).getHeight()));
+            rocks.add(rock);
+
+            boolean hitBottom = false;
+
+            while(!hitBottom){
+
+                rock.move(directionFromChar(instructions[instructionPos]), rocks);
+                hitBottom = !rock.move(Direction.D, rocks);
+
+                if(instructionPos == instructions.length -1){
+                    instructionPos =0;
+                }
+                else{
+                    instructionPos++;
+                }
+            }
+
+            if(rockType == pieces.size() -1){
+                rockType = 0;
+            }
+            else{
+                rockType++;
+            }
+
+           highestBlock = Math.min(highestBlock, rock.getCoord().getY());
+//            draw(rocks, highestBlock);
+        }
+
+
+        return Math.abs(highestBlock);
+    }
+
+    private Direction directionFromChar(char direction){
+        switch(direction){
+            case '<' -> {
+                return Direction.L;
+            }
+            case '>' -> {
+                return Direction.R;
+            }
+            default -> throw new IllegalArgumentException();
+        }
+    }
+
+    private void draw(List<TetrisRock> rocks, long lowestY){
+        for(long y = -14; y < 0; y++){
+            for(int x = 0; x <7; x++){
+                boolean drawn = false;
+                for(TetrisRock rock: rocks){
+                    for(Coord coord : rock.getOccupiedCoords()){
+                        if(coord.equals(new Coord((long) x, y))){
+                            drawn = true;
+                            System.out.print("#");
+                        }
+                    }
+                }
+                if(!drawn){
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
--- AOC/src/test/java/nl/roykovic/aoc/_2022/tetris/TetrisFactoryTest.java
+++ AOC/src/test/java/nl/roykovic/aoc/_2022/tetris/TetrisFactoryTest.java
@@ -1,4 +1,27 @@
 package nl.roykovic.aoc._2022.tetris;
 
+import nl.roykovic.aoc._2022.rucksack.Rucksack;
+import nl.roykovic.aoc._2022.rucksack.RucksackFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TetrisFactoryTest {
+    @Test
+    void testExampleHeight() throws IOException {
+        File input = new File("src/test/resources/2022/TetrisTestInput.txt");
+        assertEquals(3068, new TetrisFactory().generateFromFile(input));
+    }
+
+    @Test
+    void testActualHeight() throws IOException {
+        File input = new ClassPathResource("2022/TetrisInput.txt").getFile();
+        assertEquals(3151, new TetrisFactory().generateFromFile(input));
+    }
 }

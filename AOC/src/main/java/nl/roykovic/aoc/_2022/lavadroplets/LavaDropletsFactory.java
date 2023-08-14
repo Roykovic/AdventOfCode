f350bf611c1b918ec1d4e983c4ab997c8238e49a
Roykovic <roykovic97@outlook.com>
2023-08-14 16:48:09 +0200

    added lavadroplets inputs

---
+++ AOC/src/main/java/nl/roykovic/aoc/_2022/lavadroplets/LavaDropletsFactory.java
@@ -0,0 +1,36 @@
+package nl.roykovic.aoc._2022.lavadroplets;
+
+import nl.roykovic.aoc._2022.volcano.Valve;
+import nl.roykovic.aoc._2022.volcano.Volcano;
+import nl.roykovic.aoc.utils.Coord;
+import nl.roykovic.aoc.utils.Face;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class LavaDropletsFactory {
+    public List<Face> generateFromFile(File file) throws FileNotFoundException {
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        List<String> lines = reader.lines().toList();
+
+        List<Face> faces = new ArrayList<>();
+
+        for(String line: lines){
+
+            LavaDroplet droplet = new LavaDroplet(new Coord(line));
+
+            faces.addAll(Arrays.asList(droplet.getFaces()));
+        }
+
+    return faces.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().filter(x -> x.getValue() == 1).map(Map.Entry::getKey).toList();
+    }
+}

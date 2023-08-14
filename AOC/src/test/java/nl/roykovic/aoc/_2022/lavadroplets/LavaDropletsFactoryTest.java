fb8f04b2d3f5aa65e3b0739c70d7c724658a7d6f
Roykovic <roykovic97@outlook.com>
2023-08-14 16:50:45 +0200

    fixed files and added puzzle answer

--- AOC/src/test/java/nl/roykovic/aoc/_2022/lavadroplets/LavaDropletsFactoryTest.java
+++ AOC/src/test/java/nl/roykovic/aoc/_2022/lavadroplets/LavaDropletsFactoryTest.java
@@ -1,10 +1,8 @@
 package nl.roykovic.aoc._2022.lavadroplets;
 
-import nl.roykovic.aoc._2022.volcano.Volcano;
-import nl.roykovic.aoc._2022.volcano.VolcanoFactory;
-import nl.roykovic.aoc.utils.Coord;
 import nl.roykovic.aoc.utils.Face;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,6 +15,14 @@ public class LavaDropletsFactoryTest {
     @Test
     void testExampleSurfaceArea() throws IOException {
         File input = new File("src/test/resources/2022/LavaDropletsTestInput.txt");
+        List<Face> faces = new LavaDropletsFactory().generateFromFile(input);
+
+        assertEquals(64, faces.size());
+    }
+
+    @Test
+    void testActualSurfaceArea() throws IOException {
+        File input = new ClassPathResource("2022/LavaDropletsInput.txt").getFile();
         List<Face> faces = new LavaDropletsFactory().generateFromFile(input);
 
         assertEquals(64, faces.size());

88bba3ce6f5c9969f22d890b40cb895cd5205a51
Roykovic <roykovic97@outlook.com>
2023-08-15 16:15:13 +0200

    MonkeyMath part 2

--- AOC/src/test/java/nl/roykovic/aoc/_2022/monkeymath/MonkeyMathFactoryTest.java
+++ AOC/src/test/java/nl/roykovic/aoc/_2022/monkeymath/MonkeyMathFactoryTest.java
@@ -3,8 +3,11 @@ package nl.roykovic.aoc._2022.monkeymath;
 import nl.roykovic.aoc._2022.lavadroplets.LavaDropletsFactory;
 import nl.roykovic.aoc.utils.Face;
 import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mariuszgromada.math.mxparser.Expression;
+import org.mariuszgromada.math.mxparser.License;
+import org.mariuszgromada.math.mxparser.mXparser;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.script.ScriptEngine;
@@ -12,12 +15,18 @@ import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MonkeyMathFactoryTest {
+
+    @BeforeAll
+    static void setup(){
+        License.iConfirmNonCommercialUse("roykovic");
+    }
     @Test
     void testExampleEquation() throws IOException{
         File input = new File("src/test/resources/2022/MonkeyMathTestInput.txt");
@@ -59,14 +68,48 @@ public class MonkeyMathFactoryTest {
         Map<String, String> monkeyMap = new MonkeyMathFactory().generateFromFile(input);
 
         String equation = monkeyMap.get("root").replace('+', '=');
+        monkeyMap.put("humn", "z");
 
-        while(equation.matches(".*[a-z].*")) {
+        while(equation.matches(".*[a-y].*")) {
             for (Map.Entry<String, String> entry : monkeyMap.entrySet()) {
                 equation = StringUtils.replace(equation, entry.getKey(), " (" + entry.getValue() + ") ");
             }
         }
 
-        Expression e = new Expression(equation);
+        //the one NOT containing our variable 'z'
+        Expression rightSide = new Expression(StringUtils.substringAfter(equation, "="));
+
+        //Substracting the right side from the leftside to get ..z = 0
+        String leftSide = StringUtils.substringBefore(equation, "=") + " - " + rightSide.calculate();
+
+        //Solve for Z
+        Expression e = new Expression("solve( " + leftSide + ", z, 0, " + Integer.MAX_VALUE + ")");
         assertEquals(301, e.calculate());
+    }
+
+    @Test
+    void testActualEquationSecondWay() throws IOException {
+        File input = new ClassPathResource("2022/MonkeyMathInput.txt").getFile();
+        Map<String, String> monkeyMap = new MonkeyMathFactory().generateFromFile(input);
+
+        String equation = monkeyMap.get("root").replace('+', '=');
+        monkeyMap.put("humn", "z");
+
+        while(equation.matches(".*[a-y].*")) {
+            for (Map.Entry<String, String> entry : monkeyMap.entrySet()) {
+                equation = StringUtils.replace(equation, entry.getKey(), " (" + entry.getValue() + ") ");
+            }
+        }
+
+        //the one NOT containing our variable 'z'
+        Expression rightSide = new Expression(StringUtils.substringAfter(equation, "="));
+
+        //Substracting the right side from the leftside to get ..z = 0
+        String leftSide = StringUtils.substringBefore(equation, "=") + " - " + BigDecimal.valueOf(rightSide.calculate()).toPlainString();
+
+        //Solve for Z
+        Expression e = new Expression("solve( " + leftSide + ", z, 0, " + Long.MAX_VALUE + ")");
+
+        assertEquals(3327575724809L,Math.round(e.calculate()));
     }
 }

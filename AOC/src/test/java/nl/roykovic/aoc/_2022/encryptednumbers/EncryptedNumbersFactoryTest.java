5344ab20d326ef9f766bdd74cb2c06332b01ac09
Roykovic <roykovic97@outlook.com>
2023-08-15 13:50:57 +0200

    fixed typo

--- AOC/src/test/java/nl/roykovic/aoc/_2022/encryptednumbers/EncryptedNumbersFactoryTest.java
+++ AOC/src/test/java/nl/roykovic/aoc/_2022/encryptednumbers/EncryptedNumbersFactoryTest.java
@@ -33,6 +33,6 @@ public class EncryptedNumbersFactoryTest {
         EncryptedNumbersList list = new EncryptedNumbersFactory().generateFromFile(input);
         list.decrypt();
 
-        assertEquals(17490, list.findNthNumberAfterZero(1000) + list.findNthNumberAfterZero(2000) +list.findNthNumberAfterZero(3000));
+        assertEquals(17490, list.findNthNumberAfterZero(1000).getValue() + list.findNthNumberAfterZero(2000).getValue() +list.findNthNumberAfterZero(3000).getValue());
     }
 }

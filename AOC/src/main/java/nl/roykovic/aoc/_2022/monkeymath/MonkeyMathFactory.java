f224d47ff3833a7ba91231102a42b0a98b845908
Roykovic <roykovic97@outlook.com>
2023-08-15 15:32:30 +0200

    Monkeymath part 1

---
+++ AOC/src/main/java/nl/roykovic/aoc/_2022/monkeymath/MonkeyMathFactory.java
@@ -0,0 +1,31 @@
+package nl.roykovic.aoc._2022.monkeymath;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MonkeyMathFactory {
+    public Map<String, String> generateFromFile(File file) throws FileNotFoundException {
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        List<String> lines = reader.lines().toList();
+
+        Map<String, String> monkeyMap = new HashMap<>();
+
+        for(String line: lines){
+            String name = StringUtils.substringBefore(line, ":");
+            String operation = StringUtils.substringAfter(line, ":");
+
+            monkeyMap.put(name, operation);
+        }
+
+        return monkeyMap;
+    }
+}

96ec1a8025a21f160bd773a77fed787a818ec2f5
Roykovic <roykovic97@outlook.com>
2023-08-21 15:22:26 +0200

    changed to oneliner
    this way you dont have to save all the variables every cycle, this shaves of a couple of seconds

--- AOC/src/main/java/nl/roykovic/aoc/_2015/day4_adventcoin/MD5Encoder.java
+++ AOC/src/main/java/nl/roykovic/aoc/_2015/day4_adventcoin/MD5Encoder.java
@@ -11,14 +11,12 @@ import java.security.NoSuchAlgorithmException;
 public class MD5Encoder {
 
     public static String encode(String input) throws UnsupportedEncodingException, NoSuchAlgorithmException {
-        byte[] bytes = input.getBytes(StandardCharsets.UTF_8);
-
-        MessageDigest md = MessageDigest.getInstance("MD5");
-
-        byte[] digest = md.digest(bytes);
-
-        BigInteger bigInt = new BigInteger(1,digest);
-
-        return StringUtils.leftPad(bigInt.toString(16),32,'0');
+        return StringUtils.leftPad
+                (new BigInteger(1,
+                                MessageDigest.getInstance("MD5")
+                                        .digest(input.getBytes(StandardCharsets.UTF_8)))
+                                .toString(16),
+                        32,
+                        '0');
     }
 }

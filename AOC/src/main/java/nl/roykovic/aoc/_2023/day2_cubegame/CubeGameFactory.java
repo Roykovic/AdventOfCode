79b8ef873a85a9f5b87477c5cfe45c7819962c8d
Roykovic <roykovic97@outlook.com>
2023-12-02 21:50:51 +0100

    Logic + example

--- AOC/src/main/java/nl/roykovic/aoc/_2023/day2_cubegame/CubeGameFactory.java
+++ AOC/src/main/java/nl/roykovic/aoc/_2023/day2_cubegame/CubeGameFactory.java
@@ -4,11 +4,27 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CubeGameFactory {
-    public int generateFromFile(File file) throws FileNotFoundException {
+    public List<Map<String, Integer>> generateFromFile(File file) throws FileNotFoundException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
-        return 0;
+
+        return reader.lines()
+                .map(it -> it.split(":")[1])
+                .map(it -> it.split(";"))
+                .map(it -> Arrays.stream(it).map(str -> str.split(",")).toList())
+                .map(it -> it.stream()
+                        .map( arr -> Arrays.stream(arr)
+                                .collect(Collectors
+                                        .toMap(p -> p.trim().split(" ")[1], p -> p.trim().split(" ")[0]))
+                        ))
+                .map(it -> it
+                        .flatMap(m ->  m.entrySet().stream())
+                        .collect(Collectors.toMap(Map.Entry::getKey, m -> Integer.parseInt(m.getValue()), Integer::sum)))
+                .toList();
     }
 }
--- AOC/src/test/java/nl/roykovic/aoc/_2023/day2_cubegame/CubeGameFactoryTest.java
+++ AOC/src/test/java/nl/roykovic/aoc/_2023/day2_cubegame/CubeGameFactoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,8 +15,22 @@ public class CubeGameFactoryTest {
     @Test
     void testExampleCubeGame() throws IOException {
         File input = new File("src/test/resources/2023/CubeGameTestInput.txt");
-        int ans = new CubeGameFactory().generateFromFile(input);
+        List<Map<String, Integer>> games = new CubeGameFactory().generateFromFile(input);
 
-        assertEquals(8, ans);
+        int red = 12;
+        int green = 13;
+        int blue = 14;
+
+        int gameSum = 0;
+
+        for(int i = 0; i < games.size(); i++){
+            Map<String, Integer> currGame = games.get(i);
+
+            if(currGame.get("red") <= red && currGame.get("green") <= green && currGame.get("blue") <= blue){
+                gameSum += i+1;
+            }
+        }
+
+        assertEquals(8, gameSum);
     }
 }

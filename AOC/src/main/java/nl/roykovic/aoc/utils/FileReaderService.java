4a1b7138f7dd2850b4b5da67a19f26ce7753b884
Roykovic <roykovic97@outlook.com>
2023-12-03 15:43:18 +0100

    added filereader service

---
+++ AOC/src/main/java/nl/roykovic/aoc/utils/FileReaderService.java
@@ -0,0 +1,31 @@
+package nl.roykovic.aoc.utils;
+
+
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.stream.Stream;
+
+public class FileReaderService {
+    public static Stream<String> getLinesFromFile(int year, String filename, boolean testResource){
+
+        String path = "src/" +
+                (testResource ? "test/" : "main/") +
+                "resources/" +
+                year +
+                "/" +
+                filename;
+
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(path));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        return reader.lines();
+    }
+}

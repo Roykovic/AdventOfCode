e72511c899d70163797c71d357bd5a3f74a2ea67
Roykovic <roykovic97@outlook.com>
2023-12-04 11:27:37 +0100

    moved getwinningnumbers to seperate method

--- AOC/src/main/java/nl/roykovic/aoc/_2023/day4_scratchcards/ScratchCardFactory.java
+++ AOC/src/main/java/nl/roykovic/aoc/_2023/day4_scratchcards/ScratchCardFactory.java
@@ -11,19 +11,23 @@ import java.util.stream.Stream;
 public class ScratchCardFactory {
 
     public int generatePoints(Stream<String> input){
+        return getWinningNumbers(input)
+                .mapToInt(winningNum ->(int) Math.floor(Math.pow(2.0, winningNum-1)))
+                .sum();
+    }
+
+    private Stream<Integer> getWinningNumbers(Stream<String> input){
         return input
                 .map(inputStr -> inputStr.split(":")[1]
                         .split("\\|"))
                 .map(nums -> Arrays.stream(nums)
                         .map(num -> Arrays.stream(num
-                                .trim().split(" "))
+                                        .trim().split(" "))
                                 .filter(StringUtils::isNotBlank).toList()).toList())
                 .map(nums -> {
                     ArrayList<String> l = new ArrayList<>(nums.get(0));
                     l.retainAll(nums.get(1));
                     return l.size();
-                })
-                .mapToInt(winningNum ->(int) Math.floor(Math.pow(2.0, winningNum-1)))
-                .sum();
+                });
     }
 }

36f160e5beb49cf3bcbaf256c8d0d879564772b2
Roykovic <roykovic97@outlook.com>
2023-12-04 10:41:40 +0100

    day4 part 1

---
+++ AOC/src/main/java/nl/roykovic/aoc/_2023/day4_scratchcards/ScratchCardFactory.java
@@ -0,0 +1,29 @@
+package nl.roykovic.aoc._2023.day4_scratchcards;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ScratchCardFactory {
+
+    public int generate(Stream<String> input){
+        return input
+                .map(inputStr -> inputStr.split(":")[1]
+                        .split("\\|"))
+                .map(nums -> Arrays.stream(nums)
+                        .map(num -> Arrays.stream(num
+                                .trim().split(" "))
+                                .filter(StringUtils::isNotBlank).toList()).toList())
+                .map(nums -> {
+                    ArrayList<String> l = new ArrayList<>(nums.get(0));
+                    l.retainAll(nums.get(1));
+                    return l.size();
+                })
+                .mapToInt(winningNum ->(int) Math.floor(Math.pow(2.0, winningNum-1)))
+                .sum();
+    }
+}
---
+++ AOC/src/test/java/nl/roykovic/aoc/_2023/day4_scratchcards/ScratchCardFactoryTest.java
@@ -0,0 +1,21 @@
+package nl.roykovic.aoc._2023.day4_scratchcards;
+
+import nl.roykovic.aoc.utils.FileReaderService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ScratchCardFactoryTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "ScratchCardTestInput.txt,true,13",
+            "ScratchCardInput.txt,false,26346"
+    })
+    public void testScratchcardPoints(String filename, boolean test, int expected){
+        var input = FileReaderService.streamLinesFromFile(2023, filename, test);
+        assertEquals(expected, new ScratchCardFactory().generate(input));
+    }
+}

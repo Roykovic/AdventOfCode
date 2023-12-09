9b2446d024d60eabc7eb823eec23fc73757f1360
Roykovic <roykovic97@outlook.com>
2023-12-09 16:25:13 +0100

    added previousvalue + test

--- AOC/src/main/java/nl/roykovic/aoc/_2023/day9_oasis/OasisList.java
+++ AOC/src/main/java/nl/roykovic/aoc/_2023/day9_oasis/OasisList.java
@@ -11,15 +11,7 @@ public class OasisList {
     }
 
     public Long getNextValue(){
-        List<List<Long>> differenceLists = new ArrayList<>();
-
-        List<Long> currentList = new ArrayList<>(list);
-        while(currentList.stream().anyMatch(it -> it != 0)){
-            differenceLists.add(currentList);
-            currentList = getDifferenceList(currentList);
-        }
-
-
+        List<List<Long>> differenceLists = getDifferenceLists(list);
         for(int i = differenceLists.size() -2; i>=0; i--){
             List<Long> l1 = differenceLists.get(i+1);
             List<Long> l2 = differenceLists.get(i);
@@ -29,6 +21,30 @@ public class OasisList {
         }
 
         return differenceLists.get(0).get(differenceLists.get(0).size() -1);
+    }
+
+    public Long getPreviousValue(){
+        List<List<Long>> differenceLists = getDifferenceLists(list);
+        for(int i = differenceLists.size() -2; i>=0; i--){
+            List<Long> l1 = differenceLists.get(i+1);
+            List<Long> l2 = differenceLists.get(i);
+
+
+            l2.add(0, l2.get(0) - l1.get(0));
+        }
+
+        return differenceLists.get(0).get(0);
+    }
+
+    private List<List<Long>> getDifferenceLists(List<Long> input){
+        List<List<Long>> differenceLists = new ArrayList<>();
+
+        List<Long> currentList = new ArrayList<>(input);
+        while(currentList.stream().anyMatch(it -> it != 0)){
+            differenceLists.add(currentList);
+            currentList = getDifferenceList(currentList);
+        }
+        return differenceLists;
     }
 
     private List<Long> getDifferenceList(List<Long> originalList){
--- AOC/src/test/java/nl/roykovic/aoc/_2023/day9_oasis/OasisFactoryTest.java
+++ AOC/src/test/java/nl/roykovic/aoc/_2023/day9_oasis/OasisFactoryTest.java
@@ -13,7 +13,7 @@ public class OasisFactoryTest {
 
     @ParameterizedTest
     @CsvSource({
-//            "0 3 6 9 12 15,18",
+            "0 3 6 9 12 15,18",
             "1 3 6 10 15 21,28",
             "10 13 16 21 30 45,68"
     })
@@ -22,6 +22,20 @@ public class OasisFactoryTest {
         OasisList ol = new OasisFactory().generate(Stream.of(input)).get(0);
 
         assertEquals(expected, ol.getNextValue());
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "0 3 6 9 12 15,-3",
+            "1 3 6 10 15 21,0",
+            "10 13 16 21 30 45,5"
+    })
+    public void testPreviousValue(String input, int expected) {
+
+        OasisList ol = new OasisFactory().generate(Stream.of(input)).get(0);
+
+        assertEquals(expected, ol.getPreviousValue());
     }
 
     @ParameterizedTest

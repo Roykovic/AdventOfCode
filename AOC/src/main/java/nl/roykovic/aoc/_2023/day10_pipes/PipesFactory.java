b8e9beb6c1f32ee1d6362045813c3879a62429b0
Roykovic <roykovic97@outlook.com>
2023-12-11 11:26:21 +0100

    see last commit

--- AOC/src/main/java/nl/roykovic/aoc/_2023/day10_pipes/PipesFactory.java
+++ AOC/src/main/java/nl/roykovic/aoc/_2023/day10_pipes/PipesFactory.java
@@ -1,13 +1,16 @@
 package nl.roykovic.aoc._2023.day10_pipes;
 
 import nl.roykovic.aoc.utils.Coord;
-import nl.roykovic.aoc.utils.Direction;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PipesFactory {
 
-    public Long generate(List<String> input){
+    public Map<Coord, Pipe> generate(List<String> input){
 
         Map<Coord, Pipe> pipes = new HashMap<>();
         Pipe startPipe = null;
@@ -27,12 +30,13 @@ public class PipesFactory {
 
         assert startPipe != null;
 
+        startPipe.setStepsFromStart(0L);
+
         Pipe finalStartPipe = startPipe;
-        Coord[] startPipeNeighbours =  pipes.values().stream().filter(it -> Arrays.asList(it.neighbours).contains(finalStartPipe.getCoord())).map(Pipe::getCoord).toArray(Coord[]::new);
+        Coord[] startPipeNeighbours =  pipes.values().stream().filter(it -> Arrays.asList(it.getNeighbours()).contains(finalStartPipe.getCoord())).map(Pipe::getCoord).toArray(Coord[]::new);
         startPipe.setNeighbours(startPipeNeighbours);
 
         for(Coord currentCoord : startPipe.getNeighbours()) {
-
             Pipe currentPipe = pipes.get(currentCoord);
             Coord oldCoord = startPipe.getCoord();
             long steps = 1;
@@ -45,63 +49,6 @@ public class PipesFactory {
                 steps++;
             }
         }
-        return pipes.values().stream().map(Pipe::getStepsFromStart).filter(it -> it != Long.MAX_VALUE).max(Comparator.comparingLong(it -> it)).orElseThrow();
-    }
-
-    private static class Pipe{
-        private final char pipeChar;
-        private Coord[] neighbours;
-        private final Coord coord;
-
-        private Long stepsFromStart;
-
-        public Pipe(char pipeChar, Coord coord) {
-            this.pipeChar = pipeChar;
-            this.coord = coord;
-            this.neighbours = new Coord[2];
-            this.stepsFromStart = Long.MAX_VALUE;
-            this.setNeighbours();
-        }
-
-        public void setNeighbours(Coord[] neighbours) {
-            this.neighbours = neighbours;
-        }
-
-        private void setNeighbours(){
-            switch (pipeChar){
-                case '|' -> {neighbours[0]=coord.moveAndGet(Direction.U); neighbours[1]=coord.moveAndGet(Direction.D);}
-                case '-' -> {neighbours[0]=coord.moveAndGet(Direction.L); neighbours[1]=coord.moveAndGet(Direction.R);}
-                case 'L' -> {neighbours[0]=coord.moveAndGet(Direction.U); neighbours[1]=coord.moveAndGet(Direction.R);}
-                case 'J' -> {neighbours[0]=coord.moveAndGet(Direction.U); neighbours[1]=coord.moveAndGet(Direction.L);}
-                case '7' -> {neighbours[0]=coord.moveAndGet(Direction.D); neighbours[1]=coord.moveAndGet(Direction.L);}
-                case 'F' -> {neighbours[0]=coord.moveAndGet(Direction.D); neighbours[1]=coord.moveAndGet(Direction.R);}
-            }
-        }
-
-        public Long getStepsFromStart() {
-            return stepsFromStart;
-        }
-
-        public void setStepsFromStart(Long stepsFromStart) {
-            this.stepsFromStart = Long.min(stepsFromStart, this.stepsFromStart);
-        }
-
-        public Coord[] getNeighbours() {
-            return neighbours;
-        }
-
-        public Coord getCoord() {
-            return coord;
-        }
-
-        @Override
-        public String toString() {
-            return "Pipe{" +
-                    "pipeChar=" + pipeChar +
-                    ", neighbours=" + Arrays.toString(neighbours) +
-                    ", coord=" + coord +
-                    ", stepsFromStart=" + stepsFromStart +
-                    '}';
-        }
+        return pipes.entrySet().stream().filter(it -> it.getValue().getStepsFromStart() != Long.MAX_VALUE).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 }

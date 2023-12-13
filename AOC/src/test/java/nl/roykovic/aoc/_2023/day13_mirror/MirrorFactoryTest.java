f2ba4e3a3b11ff25c91fe22e6d01e1cee08190a3
Roykovic <roykovic97@outlook.com>
2023-12-13 11:04:04 +0100

    init day 13 using ClassGenerator

---
+++ AOC/src/test/java/nl/roykovic/aoc/_2023/day13_mirror/MirrorFactoryTest.java
@@ -0,0 +1,19 @@
+package nl.roykovic.aoc._2023.day13_mirror;
+
+import nl.roykovic.aoc.utils.FileReaderService;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class MirrorFactoryTest {
+@ParameterizedTest
+    @CsvSource({
+            "MirrorTestInput.txt,true,-1",
+            "MirrorInput.txt,false,-1",
+    })
+    public void test(String filename, boolean test, int expected) {
+        var input = FileReaderService.streamLinesFromFile(2023, filename, test);
+        var output = new MirrorFactory().generate(input);
+
+        assertEquals(expected, output);
+    }}

549864516b98b62b8eef179d462cc033b77876c1
Roykovic <roykovic97@outlook.com>
2023-12-15 14:00:57 +0100

    Disabled cycles for now

--- src/test/java/nl/roykovic/aoc/_2023/day14_rocks/RocksFactoryTest.java
+++ src/test/java/nl/roykovic/aoc/_2023/day14_rocks/RocksFactoryTest.java
@@ -1,19 +1,35 @@
 package nl.roykovic.aoc._2023.day14_rocks;
 
 import nl.roykovic.aoc.utils.FileReaderService;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RocksFactoryTest {
-@ParameterizedTest
+    @ParameterizedTest
     @CsvSource({
             "RocksTestInput.txt,true,136",
             "RocksInput.txt,false,109385",
     })
     public void test(String filename, boolean test, int expected) {
         var input = FileReaderService.getFileAsString(2023, filename, test);
-        var output = new RocksFactory().generate(input,true);
+        var output = new RocksFactory().generate(input, false);
 
         assertEquals(expected, output);
-    }}
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({
+            "RocksTestInput.txt,true,136",
+            "RocksInput.txt,false,109385",
+    })
+    public void testCycles(String filename, boolean test, int expected) {
+        var input = FileReaderService.getFileAsString(2023, filename, test);
+        var output = new RocksFactory().generate(input, true);
+
+        assertEquals(expected, output);
+    }
+}

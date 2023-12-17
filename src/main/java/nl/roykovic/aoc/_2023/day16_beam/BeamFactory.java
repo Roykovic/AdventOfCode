8165ccf6aff044153847ddd0686789900659cb28
Roykovic <roykovic97@outlook.com>
2023-12-17 19:53:15 +0100

    changed to list
    Sometimes you can get two of the same coords, but with different directions. With a hashmap you overwrite the first with the second. This led to wrong answer in p1

--- src/main/java/nl/roykovic/aoc/_2023/day16_beam/BeamFactory.java
+++ src/main/java/nl/roykovic/aoc/_2023/day16_beam/BeamFactory.java
@@ -5,6 +5,7 @@ import nl.roykovic.aoc._2023.day16_beam.mirrors.MirrorFactory;
 import nl.roykovic.aoc.utils.Coord;
 import nl.roykovic.aoc.utils.Direction;
 
+import javax.print.attribute.standard.Destination;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -21,11 +22,14 @@ public class BeamFactory {
 
         Coord oldCoord = new Coord(-1,0);
 
-        Map<Coord, Direction> coordsToCheck = Map.of(oldCoord.moveAndGet(Direction.R), Direction.R);
-        Map<Coord, Direction> currentCoords;
+        List<Map.Entry<Coord, Direction>> coordsToCheck = List.of(Map.entry(oldCoord.moveAndGet(Direction.R), Direction.R));
+        List<Map.Entry<Coord, Direction>> currentCoords;
         while(!coordsToCheck.isEmpty()){
-            currentCoords = new HashMap<>();
-            for(Map.Entry<Coord, Direction> e : coordsToCheck.entrySet()){
+            currentCoords = new ArrayList<>();
+            for(Map.Entry<Coord, Direction> e : coordsToCheck){
+                if(e.getKey().equals(new Coord(36,2))){
+                    System.out.println();
+                }
                 if(visitedCoords.contains(e)){
                     continue;
                 }
@@ -39,15 +43,59 @@ public class BeamFactory {
                         inputGrid[Math.toIntExact(newCoord.getY())]
                                 [Math.toIntExact(newCoord.getX())], newCoord);
 
-                currentCoords.putAll(currentMirror.move(currentDir).entrySet().stream()
+                currentCoords.addAll(currentMirror.move(currentDir).entrySet().stream()
                         .filter(it -> {
                             Coord coord = it.getKey();
                             return (coord.getX() >= 0 && coord.getX() < inputGrid[0].length) &&
                                     (coord.getY() >= 0 && coord.getY() < inputGrid.length);
-                        }).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+                        }).toList());
             }
             coordsToCheck = currentCoords;
         }
+
+        drawGrid(visitedCoords);
         return visitedCoords.stream().map(Map.Entry::getKey).distinct().toList().size();
+    }
+
+    public void drawGrid(List<Map.Entry<Coord, Direction>> lissie){
+        for(int y = 0; y< inputGrid.length; y++){
+            for(int x = 0; x< inputGrid[y].length; x++){
+                int finalX = x;
+                int finalY = y;
+                var foundCoords = lissie.stream().filter(it -> it.getKey().getX() == finalX && it.getKey().getY() == finalY).toList();
+
+                if(foundCoords.size() > 0 && inputGrid[y][x] == '.'){
+                    if(foundCoords.size() > 1){
+                        System.out.print(foundCoords.size());
+                    }
+                    else{
+                        System.out.print(getCharFromDir(foundCoords.get(0).getValue()));
+                    }
+                }
+                else {
+                    System.out.print(inputGrid[y][x]);;
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+    private char getCharFromDir(Direction d){
+        switch (d){
+            case R -> {
+                return '>';
+            }
+            case L -> {
+                return '<';
+            }
+            case U -> {
+                return '^';
+            }
+            case D -> {
+                return 'v';
+            }
+        }
+        return 0;
     }
 }

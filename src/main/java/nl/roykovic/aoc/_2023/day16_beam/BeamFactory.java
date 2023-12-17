7cfe37cacbd83ce3c5dd76189ed0c4b0029c2efb
Roykovic <roykovic97@outlook.com>
2023-12-17 12:10:03 +0100

    day 16 init using classgenerator

---
+++ src/main/java/nl/roykovic/aoc/_2023/day16_beam/BeamFactory.java
@@ -0,0 +1,8 @@
+package nl.roykovic.aoc._2023.day16_beam;
+
+import java.util.stream.Stream;
+public class BeamFactory {
+    public int generate(Stream<String> input) {
+        return 0;
+    }
+}
--- src/main/java/nl/roykovic/aoc/utils/ClassGenerator.java
+++ src/main/java/nl/roykovic/aoc/utils/ClassGenerator.java
@@ -11,7 +11,7 @@ import java.util.Locale;
 public class ClassGenerator {
 
     public static void main(String[] args) {
-        args = new String[]{"15", "ascii"};
+        args = new String[]{"16", "beam"};
 
         
         if (args.length < 2 || args.length > 3) {

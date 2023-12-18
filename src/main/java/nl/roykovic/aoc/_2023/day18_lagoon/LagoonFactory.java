633e6360f2b586033083d1cf588c8ff84e5c99d0
Roykovic <roykovic97@outlook.com>
2023-12-18 19:15:22 +0100

    Reading part 2; the color was a red herring :sweat_smile:

--- src/main/java/nl/roykovic/aoc/_2023/day18_lagoon/LagoonFactory.java
+++ src/main/java/nl/roykovic/aoc/_2023/day18_lagoon/LagoonFactory.java
@@ -14,45 +14,23 @@ public class LagoonFactory {
     public int generate(Stream<String> input) {
 
         Coord start = new Coord(0,0);
-        List<TrenchCoord> trenches = new ArrayList<>();
+        List<Coord> trenches = new ArrayList<>();
 
         for(String[] arr : input.map(it -> it.split(" ")).toList()){
-            List<TrenchCoord> coords = new ArrayList<>();
+            List<Coord> coords = new ArrayList<>();
             for(int i =0; i< Integer.parseInt(arr[1]); i++){
                 start.move(Direction.valueOf(arr[0]));
-                coords.add(new TrenchCoord(new Coord(start), Color.decode(arr[2].replace("(", "").replace(")", ""))));
+                coords.add(new Coord(start));
             }
             trenches.addAll(coords);
         }
 
         //Picks theorem again, we can calculate area, we have boundary points. We want to know Interior points + boundary points:
 //        drawGrid(trenches);
-        var area = Utils.shoelaceArea(trenches.stream().map(TrenchCoord::coord).toList());
+        var area = Utils.shoelaceArea(trenches);
         var interiorPoints = Math.abs(((double) trenches.size() /2) -1 -area);
 
         return (int) (trenches.size()+interiorPoints);
 
-    }
-
-    private void drawGrid(List<TrenchCoord> coords){
-        long maxX = coords.stream().mapToLong(it -> it.coord.getX()).max().orElseThrow();
-        long maxY = coords.stream().mapToLong(it -> it.coord.getY()).max().orElseThrow();
-
-        for(int y = 0; y<= maxY; y++){
-            for(int x = 0; x <= maxX; x++){
-                int finalX = x;
-                int finalY = y;
-                if(coords.stream().anyMatch(it -> Objects.equals(it.coord, new Coord(finalX, finalY)))){
-                    System.out.printf("#");
-                }
-                else {
-                    System.out.printf(".");
-                }
-            }
-            System.out.println();
-        }
-    }
-
-    private record TrenchCoord(Coord coord, Color color) {
     }
 }

03a124c510ec92f4b6be196a476031b5c555d4cc
Roykovic <roykovic97@outlook.com>
2024-10-24 16:38:40 +0200

    init day 21

---
+++ src/main/java/nl/roykovic/aoc/_2015/day21_bossfight/BossfightFactory.java
@@ -0,0 +1,8 @@
+package nl.roykovic.aoc._2015.day21_bossfight;
+
+import java.util.stream.Stream;
+public class BossfightFactory {
+    public int generate(Stream<String> input) {
+        return 0;
+    }
+}

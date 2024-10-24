251f70258c1ee29ffea332b092e9cbea2d14eaa3
Roykovic <roykovic97@outlook.com>
2024-10-24 15:25:48 +0200

    WIP p2

--- src/main/java/nl/roykovic/aoc/_2015/day19_molecules/MoleculeFactory.java
+++ src/main/java/nl/roykovic/aoc/_2015/day19_molecules/MoleculeFactory.java
@@ -1,5 +1,6 @@
 package nl.roykovic.aoc._2015.day19_molecules;
 
+import nl.roykovic.aoc._2022.day12_hillclimb.Node;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.BufferedReader;
@@ -9,6 +10,8 @@ import java.io.FileReader;
 import java.util.*;
 
 public class MoleculeFactory {
+
+    private List<String> triedMolecules = new ArrayList<>();
     public int generateFromFile(File file) throws FileNotFoundException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         List<String> lines = reader.lines().toList();
@@ -29,6 +32,28 @@ public class MoleculeFactory {
             mutations.get(parts[0]).add(parts[1]);
         }
         return calibrate(molecule, mutations).size();
+    }
+
+    public int generateNodesFromFile(List<String> lines){
+        String molecule = lines.get(lines.size() -1);
+
+        Map<String, List<String>> mutations = new HashMap<>();
+
+        for(String line : lines){
+            if(StringUtils.isBlank(line)){
+                break;
+            }
+            String[] parts = StringUtils.splitByWholeSeparator(line, " => ");
+
+            if(!mutations.containsKey(parts[0])){
+                mutations.put(parts[0], new ArrayList<>());
+            }
+            mutations.get(parts[0]).add(parts[1]);
+        }
+
+        List<Integer> results = new ArrayList<>();
+        fabricate(results, "e", molecule,mutations,0);
+        return results.stream().mapToInt(it -> it).min().orElseThrow();
     }
 
     private HashSet<String> calibrate(String molecule, Map<String, List<String>> mutations) {
@@ -52,5 +77,31 @@ public class MoleculeFactory {
         }
 
         return possibleMolecules;
+    }
+
+    private void fabricate(List<Integer> results, String molecule, String expected, Map<String, List<String>> mutations, int step) {
+        if(Objects.equals(molecule, expected)){
+            results.add(step);
+        }
+
+        for (var entry : mutations.entrySet()) {
+            for (String mutation : entry.getValue()) {
+                int lastIndex = 0;
+                while (lastIndex != -1) {
+
+                    lastIndex = molecule.indexOf(entry.getKey(), lastIndex);
+
+                    if (lastIndex != -1) {
+                        String newMolecule = molecule.substring(0,lastIndex) + mutation + molecule.substring(lastIndex +entry.getKey().length());
+
+                       if(newMolecule.length() <= expected.length() && !triedMolecules.contains(newMolecule)) {
+                           triedMolecules.add(newMolecule);
+                           fabricate(results, newMolecule, expected, mutations, step + 1);
+                       }
+                        lastIndex++;
+                    }
+                }
+            }
+        }
     }
 }
--- src/test/java/nl/roykovic/aoc/_2015/day19_molecules/MoleculeFactoryTest.java
+++ src/test/java/nl/roykovic/aoc/_2015/day19_molecules/MoleculeFactoryTest.java
@@ -1,10 +1,15 @@
 package nl.roykovic.aoc._2015.day19_molecules;
 
+import nl.roykovic.aoc._2015.day15_ingredients.IngredientsFactory;
+import nl.roykovic.aoc.utils.FileReaderService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -31,8 +36,22 @@ public class MoleculeFactoryTest {
         File input = new ClassPathResource("2015/MoleculeInput.txt").getFile();
         int combinations = new MoleculeFactory().generateFromFile(input);
 
-        assert(combinations < 603); //603 is wrong; answer is too high
-        assert(combinations < 581); //581 is wrong; answer is too high
+        assert (combinations < 603); //603 is wrong; answer is too high
+        assert (combinations < 581); //581 is wrong; answer is too high
         assertEquals(509, combinations);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "MoleculeExampleInput.txt,true,3",
+            "MoleculeSecondExampleInput.txt,true,6",
+            "MoleculeInput.txt,false,-1",
+    })
+    public void testFabrication(String filename, boolean test, int expected) {
+        var input = FileReaderService.getLinesFromFile(2015, filename, test);
+        var output = new MoleculeFactory().generateNodesFromFile(input);
+
+        assert(output < 467); //answer too high
+        assertEquals(expected, output);
     }
 }

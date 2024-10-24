1608f5d34ac98eb98f717f605197b27adced690c
Roykovic <roykovic97@outlook.com>
2024-10-24 16:41:02 +0200

    startup 21

---
+++ src/main/java/nl/roykovic/aoc/_2015/day21_bossfight/Player.java
@@ -0,0 +1,31 @@
+package nl.roykovic.aoc._2015.day21_bossfight;
+
+public class Player {
+    private int hitPoints;
+    private int damage;
+    private int armor;
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+}

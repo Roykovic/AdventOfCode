daec51381fe756907a4767ae6ad5887da807a8ef
Roykovic <roykovic97@outlook.com>
2024-10-25 11:09:23 +0200

    init d22

---
+++ src/main/java/nl/roykovic/aoc/_2015/day22_magicbossfight/MagicbossfightFactory.java
@@ -0,0 +1,8 @@
+package nl.roykovic.aoc._2015.day22_magicbossfight;
+
+import java.util.stream.Stream;
+public class MagicbossfightFactory {
+    public int generate(Stream<String> input) {
+        return 0;
+    }
+}

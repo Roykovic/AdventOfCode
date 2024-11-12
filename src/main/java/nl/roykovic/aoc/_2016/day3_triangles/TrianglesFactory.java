16fb8320af2e7ffeb42a1b260aa25ebe75b65f15
Roykovic <roykovic97@outlook.com>
2024-11-12 20:55:57 +0100

    p1 & 2

--- src/main/java/nl/roykovic/aoc/_2016/day3_triangles/TrianglesFactory.java
+++ src/main/java/nl/roykovic/aoc/_2016/day3_triangles/TrianglesFactory.java
@@ -1,8 +1,50 @@
 package nl.roykovic.aoc._2016.day3_triangles;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 public class TrianglesFactory {
     public int generate(Stream<String> input) {
-        return 0;
+        return (int) input
+                .map(String::trim)
+                .map(it -> it.split(" "))
+                .map(it -> Arrays.stream(it).filter(s -> !s.isEmpty()).toArray(String[]::new))
+                .map(this::isValid).filter(it -> it).count();
+    }
+
+    public int generateByColumn(Stream<String> input){
+        var grid =input
+                .map(String::trim)
+                .map(it -> it.split(" "))
+                .map(it -> Arrays.stream(it).filter(s -> !s.isEmpty()).toArray(String[]::new))
+                .toArray(String[][]::new);
+
+        int counter = 0;
+
+        for(int x = 0; x < grid[0].length; x++) {
+            for (int y = 0; y + 2 < grid.length; y+=3) {
+
+                String[] curArr = new String[3];
+
+                curArr[0] = grid[y][x];
+                curArr[1] = grid[y+1][x];
+                curArr[2] = grid[y+2][x];
+
+                if(isValid(curArr)){
+                    counter++;
+                }
+
+            }
+        }
+
+
+        return counter;
+    }
+
+    private boolean isValid(String[] triangleArray){
+        int firstSide = Integer.parseInt(triangleArray[0]);
+        int secondSide = Integer.parseInt(triangleArray[1]);
+        int thirdSide = Integer.parseInt(triangleArray[2]);
+
+        return firstSide + secondSide > thirdSide && thirdSide + secondSide > firstSide && firstSide + thirdSide > secondSide;
     }
 }
--- src/test/java/nl/roykovic/aoc/_2016/day3_triangles/TrianglesFactoryTest.java
+++ src/test/java/nl/roykovic/aoc/_2016/day3_triangles/TrianglesFactoryTest.java
@@ -8,13 +8,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TrianglesFactoryTest {
     @ParameterizedTest
     @CsvSource({
-            "TrianglesTestInput.txt,true,-1",
-            "TrianglesInput.txt,false,-1",
+            "TrianglesTestInput.txt,true,0",
+            "TrianglesInput.txt,false,862",
     })
     public void test(String filename, boolean test, int expected) {
         var input = FileReaderService.streamLinesFromFile(2016, filename, test);
         var output = new TrianglesFactory().generate(input);
 
+        assertEquals(expected, output);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "TrianglesInput.txt,false,1577",
+    })
+    public void testByColumn(String filename, boolean test, int expected) {
+        var input = FileReaderService.streamLinesFromFile(2016, filename, test);
+        var output = new TrianglesFactory().generateByColumn(input);
+
+        assert(output < 3407); //answer too high
         assertEquals(expected, output);
     }
 }

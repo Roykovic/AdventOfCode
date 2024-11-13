3508a3c5e6d5382afb645afa7229fff47db45bcb
Roykovic <roykovic97@outlook.com>
2024-11-13 16:04:40 +0100

    p1

--- src/main/java/nl/roykovic/aoc/_2016/day9_formatcompression/FormatCompressionFactory.java
+++ src/main/java/nl/roykovic/aoc/_2016/day9_formatcompression/FormatCompressionFactory.java
@@ -1,8 +1,32 @@
 package nl.roykovic.aoc._2016.day9_formatcompression;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.stream.Stream;
 public class FormatCompressionFactory {
     public int generate(Stream<String> input) {
         return 0;
+    }
+
+    public String decompress(String input){
+        if(!input.contains("(")){
+            return input;
+        }
+        String result = "";
+
+        while(input.contains("(")) {
+            result += StringUtils.substringBefore(input, "(");
+            String operator = StringUtils.substringBetween(input, "(", ")");
+            int numberOfChars = Integer.parseInt(StringUtils.substringBefore(operator, "x"));
+            int repetitions = Integer.parseInt(StringUtils.substringAfter(operator, "x"));
+
+            input = StringUtils.substringAfter(input, ")");
+            for(int i = 0; i< repetitions; i++){
+                result += input.substring(0, numberOfChars);
+            }
+            input = input.substring(numberOfChars);
+        }
+
+        return result + input;
     }
 }
--- src/test/java/nl/roykovic/aoc/_2016/day9_formatcompression/FormatCompressionFactoryTest.java
+++ src/test/java/nl/roykovic/aoc/_2016/day9_formatcompression/FormatCompressionFactoryTest.java
@@ -8,13 +8,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FormatCompressionFactoryTest {
     @ParameterizedTest
     @CsvSource({
-            "FormatCompressionTestInput.txt,true,-1",
-            "FormatCompressionInput.txt,false,-1",
+            "FormatCompressionInput.txt,false,110346",
     })
     public void test(String filename, boolean test, int expected) {
-        var input = FileReaderService.streamLinesFromFile(2016, filename, test);
-        var output = new FormatCompressionFactory().generate(input);
+        var input = FileReaderService.getFileAsString(2016, filename, test);
+        var output = new FormatCompressionFactory().decompress(input);
+
+        assertEquals(expected, output.replace(" ", "").length());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "ADVENT,ADVENT,6",
+            "A(1x5)BC,ABBBBBC,7",
+            "(3x3)XYZ,XYZXYZXYZ,9",
+            "A(2x2)BCD(2x2)EFG,ABCBCDEFEFG,11",
+            "(6x1)(1x3)A,(1x3)A,6",
+            "X(8x2)(3x3)ABCY,X(3x3)ABC(3x3)ABCY,18"
+
+    })
+    public void test(String input, String expected, int length) {
+        var output = new FormatCompressionFactory().decompress(input);
 
         assertEquals(expected, output);
+        assertEquals(length, output.length());
     }
 }

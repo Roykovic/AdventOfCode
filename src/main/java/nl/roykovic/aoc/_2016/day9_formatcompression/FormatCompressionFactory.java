14adda936f0a55bf87eaef63ceb4765cba271c9a
Roykovic <roykovic97@outlook.com>
2024-11-14 11:05:48 +0100

    p2

--- src/main/java/nl/roykovic/aoc/_2016/day9_formatcompression/FormatCompressionFactory.java
+++ src/main/java/nl/roykovic/aoc/_2016/day9_formatcompression/FormatCompressionFactory.java
@@ -29,4 +29,50 @@ public class FormatCompressionFactory {
 
         return result + input;
     }
+
+    public int decompressFake(String input){
+        if(!input.contains("(")){
+            return input.length();
+        }
+        String result = "";
+
+        while(input.contains("(")) {
+            result += StringUtils.substringBefore(input, "(");
+            String operator = StringUtils.substringBetween(input, "(", ")");
+            int numberOfChars = Integer.parseInt(StringUtils.substringBefore(operator, "x"));
+            int repetitions = Integer.parseInt(StringUtils.substringAfter(operator, "x"));
+
+            input = StringUtils.substringAfter(input, ")");
+            for(int i = 0; i< repetitions; i++){
+                result += input.substring(0, numberOfChars);
+            }
+            input = input.substring(numberOfChars);
+        }
+
+        var hans = result + input;
+
+        return 0;
+    }
+
+    public long countResult(String input){
+        if(!input.contains("(")){
+            return input.length();
+        }
+
+        long result = StringUtils.substringBefore(input, "(").length();
+
+        String operator = StringUtils.substringBetween(input, "(", ")");
+        int numberOfChars = Integer.parseInt(StringUtils.substringBefore(operator, "x"));
+        int repetitions = Integer.parseInt(StringUtils.substringAfter(operator, "x"));
+
+        input = StringUtils.substringAfter(input, ")");
+        for(int i = 0; i< repetitions; i++){
+            result+=countResult(input.substring(0,numberOfChars));
+        }
+        input = input.substring(numberOfChars);
+
+        result+=countResult(input);
+
+        return result;
+    }
 }
--- src/test/java/nl/roykovic/aoc/_2016/day9_formatcompression/FormatCompressionFactoryTest.java
+++ src/test/java/nl/roykovic/aoc/_2016/day9_formatcompression/FormatCompressionFactoryTest.java
@@ -19,6 +19,17 @@ public class FormatCompressionFactoryTest {
 
     @ParameterizedTest
     @CsvSource({
+            "FormatCompressionInput.txt,false,10774309173",
+    })
+    public void test2(String filename, boolean test, long expected) {
+        var input = FileReaderService.getFileAsString(2016, filename, test);
+        var output = new FormatCompressionFactory().countResult(input);
+
+        assertEquals(expected, output);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
             "ADVENT,ADVENT,6",
             "A(1x5)BC,ABBBBBC,7",
             "(3x3)XYZ,XYZXYZXYZ,9",
@@ -32,5 +43,19 @@ public class FormatCompressionFactoryTest {
 
         assertEquals(expected, output);
         assertEquals(length, output.length());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "(3x3)XYZ,9",
+            "X(8x2)(3x3)ABCY,20",
+            "(27x12)(20x12)(13x14)(7x10)(1x12)A,241920",
+            "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN,445",
+
+    })
+    public void test(String input, int length) {
+        var output = new FormatCompressionFactory().countResult(input);
+
+        assertEquals(length, output);
     }
 }

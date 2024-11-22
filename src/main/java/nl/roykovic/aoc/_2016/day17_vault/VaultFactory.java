3f89b466e86e52c1736ed99d0f972a5d9f5fa485
Roykovic <roykovic97@outlook.com>
2024-11-22 14:55:24 +0100

    Update VaultFactory.java

--- src/main/java/nl/roykovic/aoc/_2016/day17_vault/VaultFactory.java
+++ src/main/java/nl/roykovic/aoc/_2016/day17_vault/VaultFactory.java
@@ -2,7 +2,7 @@ package nl.roykovic.aoc._2016.day17_vault;
 
 import java.util.stream.Stream;
 public class VaultFactory {
-    public int generate(Stream<String> input) {
+    public int generate(String input) {
         return 0;
     }
 }
